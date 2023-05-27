01a2188b9a6c9c6aac03c947bb9655f2db7d5b9f
Mark Liman <65871508+StacLigasfolf@users.noreply.github.com>
2023-05-27 20:50:11 +0300

    Lesson_11 is ready

--- Lesson_10/common/variables.py
+++ Lesson_10/common/variables.py
@@ -1,6 +1,7 @@
 import logging
 
-DEFAULT_PORT = 70000
+
+DEFAULT_PORT = 7777
 
 DEFAULT_IP_ADDRESS = '127.0.0.1'
 
@@ -12,12 +13,16 @@ ENCODING = 'utf-8'
 
 LOGGING_LEVEL = logging.DEBUG
 
+SERVER_DATABASE = 'sqlite:///server_base.db3'
+
+
 ACTION = 'action'
 TIME = 'time'
 USER = 'user'
 ACCOUNT_NAME = 'account_name'
 SENDER = 'from'
 DESTINATION = 'to'
+
 
 PRESENCE = 'presence'
 RESPONSE = 'response'
@@ -26,9 +31,11 @@ MESSAGE = 'message'
 MESSAGE_TEXT = 'mess_text'
 EXIT = 'exit'
 
+
 RESPONSE_200 = {RESPONSE: 200}
 
 RESPONSE_400 = {
-    RESPONSE: 400,
-    ERROR: None
-}
+            RESPONSE: 400,
+            ERROR: None
+        }
+
